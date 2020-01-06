cb54e4ca1092fa484925ae7aaf26dbcb4d238d64
iolumbaram <hazel77744@gmail.com>
2020-01-06 10:22:49 +0800

    removed un-used projs

--- LegacyManager/LegacyUI.cs
+++ LegacyManager/LegacyUI.cs
@@ -26,24 +26,32 @@ namespace LegacyManager
 
             Task.Run(() =>
             {
-                var factory = new ConnectionFactory() { HostName = "localhost" };
-                var connection = factory.CreateConnection();
-                var channel = connection.CreateModel();
-                channel.ExchangeDeclare(exchange: "test", type: ExchangeType.Topic);
-
-                var queueName = channel.QueueDeclare().QueueName;
-                channel.QueueBind(queue: queueName, exchange: "test", routingKey: "hello");
-
-                Console.WriteLine(" [*] Waiting for logs.");
-
-                var consumer = new EventingBasicConsumer(channel);
-                consumer.Received += (model, ea) =>
+                try
                 {
-                    var body = ea.Body;
-                    var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] {0}", message);
-                };
-                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                    var factory = new ConnectionFactory() { HostName = "localhost" };
+                    var connection = factory.CreateConnection();
+                    var channel = connection.CreateModel();
+                    channel.ExchangeDeclare(exchange: "test", type: ExchangeType.Topic);
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queueName, exchange: "test", routingKey: "hello");
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+                }
+                catch
+                {
+                    Console.WriteLine("Error Sub");
+                }
+
             });
         }
     }
